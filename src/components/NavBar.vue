54f189bcd2351ff65e268b850e1411f833789afc
Ro-off <romansk2001@gmail.com>
2025-06-23 20:10:44 +0300

    Add NavBar component and integrate it into App.vue

---
+++ src/components/NavBar.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { useCharacterStore } from "../stores/CharactersStore";
+
+const characterStore = useCharacterStore();
+</script>
+<template>
+  <nav
+    class="flex justify-between p-3 pt-10 pb-6 items-center pl-6 fixed z-10 blur-progresive w-full"
+  >
+    <h1
+      class="font-starwrs font-bold text-2xl md:text-3xl lg:text-4xl pl- text-amber-400"
+    >
+      Star Wars characters
+    </h1>
+    <input
+      type="text"
+      placeholder="Search..."
+      class="input input-ghost text-amber-400 placeholder:text-amber-400 focus:bg-transparent focus:outline-0"
+      v-model="characterStore.filter"
+    />
+  </nav>
+  <div class="h-30"></div>
+</template>
+
+<style>
+.blur-progresive {
+  overflow: hidden;
+}
+.blur-progresive::before {
+  content: "";
+  position: absolute;
+  inset: 0;
+  z-index: 0;
+  pointer-events: none;
+  mask-image: linear-gradient(black, black, rgba(0, 0, 0, 0.814), transparent);
+  backdrop-filter: blur(8px);
+  -webkit-backdrop-filter: blur(8px);
+}
+.blur-progresive > * {
+  position: relative;
+  z-index: 1;
+}
+</style>
--- src/pages/App.vue
+++ src/pages/App.vue
@@ -1,34 +1,13 @@
 <script setup lang="ts">
-import { watch } from "vue";
 import CharacterCard from "../components/CharacterCard.vue";
 import { useCharacterStore } from "../stores/CharactersStore";
+import NavBar from "../components/NavBar.vue";
 
 const characterStore = useCharacterStore();
-
-watch(
-  () => characterStore.$state.filter,
-  async (newFilter2) => {
-    console.log("🚀 ~ newFilter2:", newFilter2);
-  }
-);
 </script>
 
 <template>
-  <nav
-    class="flex justify-between p-3 pt-10 pb-6 items-center pl-6 bg-gradient-to-b from-base-100 to-transparent"
-  >
-    <h1
-      class="font-starwrs font-bold text-2xl md:text-3xl lg:text-4xl pl- text-amber-400"
-    >
-      Star Wars characters
-    </h1>
-    <input
-      type=" text"
-      placeholder="Search..."
-      className="input input-ghost text-amber-400 placeholder:text-amber-400"
-      v-model="characterStore.filter"
-    />
-  </nav>
+  <NavBar />
   <div class="p-3 flex gap-5 flex-wrap justify-around">
     <CharacterCard
       v-for="(character, index) in characterStore.getCharacters"
